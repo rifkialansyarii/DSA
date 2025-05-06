997d673a15521b022b893278cbf63f38762151f5
rifkialansyarii <alansyari3431@gmail.com>
2025-05-06 14:57:12 +0800

    modifikasi menu utama, dan menambahkan sepatah kata

--- utsC030324105/menu.cpp
+++ utsC030324105/menu.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void dMenu(){
 system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"Aplikasi Sorting Insertion"<<"\n";       
+cout<<"1. Memasukkan data"<<"\n";            
+cout<<"2. Menampilkan data"<<"\n";            
+cout<<"3. Sorting"<<"\n";           
+cout<<"4. Sepatah kata"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -21,6 +21,11 @@ cout<<"hallo saya menu "<<pesan;
 getch();
 }
 
+void sepatahKata(){
+  system("cls");
+  cout<<"Indonesia Bisa, Kita Juara";
+  getch();
+}
 
 int main() {
 char pl;
@@ -43,7 +48,7 @@ do
     /* code */
     break;  
    case '4':
-    mPertama("ke- empat");
+    sepatahKata();
     /* code */
     break;  
   case '5':
