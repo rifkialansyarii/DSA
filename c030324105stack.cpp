9f6d686c60b628f034607ade3238e098a2bdb985
rifkialansyarii <alansyari3431@gmail.com>
2025-05-27 20:07:01 +0800

    Menambahkan prosedur InitializeStack

--- c030324105stack.cpp
+++ c030324105stack.cpp
@@ -9,9 +9,16 @@ typedef struct{
 
 }Stack;
 
+void InitializeStack(Stack* S);
+
 int main(){
     Stack Stack1;
     Stack* S = &Stack1;
 
+
     return 0;
+}
+
+void InitializeStack(Stack* S){
+    S->Count = 0;
 }
