10219f09556481611b1d235a7ebe45ae31dd20c0
rifkialansyarii <alansyari3431@gmail.com>
2025-05-29 16:46:49 +0800

    membuat prosedur pop dan push

--- c030324105stack.cpp
+++ c030324105stack.cpp
@@ -11,13 +11,18 @@ typedef struct{
 
 void InitializeStack(Stack* S);
 int Full(Stack* S);
+int Empty(Stack* S);
+void Pop(Stack* S, ItemType *x);
+void Push(ItemType x, Stack *S);
 
 int main(){
     Stack Stack1;
     Stack* S = &Stack1;
 
 
-    cout << S->Count;
+    InitializeStack(S);
+    
+
 
     return 0;
 }
@@ -32,4 +37,22 @@ int Full(Stack* S){
 
 int Empty(Stack *S){
     return (S->Count == 0);
+}
+
+void Pop(Stack* S, ItemType *x){
+    if (Empty(S)){
+        cout << "Stack masih kosong!" << endl;
+    }else{
+        --(S->Count);
+        *x = S->Item[S->Count];
+    }
+}
+
+void Push(ItemType x, Stack *S){
+    if (Full(S)){
+        cout << "Stack Penuh! Data tidak dapat masuk!" << endl;
+    }else{
+        S->Item[S->Count] = x;
+        ++(S->Count);
+    }
 }
