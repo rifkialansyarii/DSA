c63a27898284d560aeb6b4883ef462301b6466d1
rifkialansyarii <alansyari3431@gmail.com>
2025-05-29 17:02:56 +0800

    Membuat program stack push and pop

--- c030324105stack.cpp
+++ c030324105stack.cpp
@@ -20,14 +20,18 @@ void Push(ItemType x, Stack *S);
 int main(){
     Stack Stack1;
     Stack* S = &Stack1;
-
+    
+    ItemType resultpop;
 
     InitializeStack(S);
     Push(13, S);
     Push(70, S);
-
-    
-
+    Push(80, S);
+    Push(100, S);
+    Pop(S, &resultpop);
+    for(int i = (S -> Count) - 1; i >= 0; --i){
+        cout << S -> Item[i] << endl;
+    }
 
     return 0;
 }
