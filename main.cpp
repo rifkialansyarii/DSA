96a7788cbfd2584f4c4be723ecc5fbfa9434392f
rifkialansyarii <alansyari3431@gmail.com>
2025-06-21 16:54:34 +0800

    Menambahkan fitur addItem

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct Node{
+    int id;
+    std::string itemName;
+    std::string category;
+    int stock;
+
+    struct Node *next;
+};
+
+void addItem(Node *&head, int id, const std::string &itemName, const std::string &category, int stock);
+
+int main(){
+    Node *inventory = nullptr;
+
+    addItem(inventory, 1, "Mikrotik", "Router", 10);
+
+    Node *current = inventory;
+    while(current != nullptr){
+        std::cout << "No: " << current->id << " | ";
+        std::cout << "Name: " << current->itemName << " | ";
+        std::cout << "Category: " << current->category << " | ";
+        std::cout << "Stock: " << current->stock << std::endl;
+
+        current = current->next;
+    }
+
+}
+
+
+void addItem(Node *&head, int id, const std::string &itemName, const std::string &category, int stock){
+    Node *newNode = new Node{id, itemName, category, stock, nullptr};
+
+    if (head == nullptr){
+        head = newNode;
+    }
+
+    Node *current = head;
+    while(current->next != nullptr){
+        current = current->next;
+    }
+
+    current->next = newNode;
+
+
+};
+
